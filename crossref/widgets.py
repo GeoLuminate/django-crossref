8eea46ea9c3f0a7a12d46ecf8960e40265b9f254
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-17 11:04:35 +0100

    PDF widget for admin

--- crossref/widgets.py
+++ crossref/widgets.py
@@ -1,30 +1,28 @@
-# from django_select2.forms import Select2Mixin
-from django import forms
 from django.forms import widgets
 
-class CrossRefWorkWidget():
-    pass
+from django.forms.widgets import ClearableFileInput
 
-class CrossRefFunderWidget():
-    pass
+
+class AdminPDFWidget(ClearableFileInput):
+    template_name = 'admin/pdf_file_input.html'
+
 
 class PagesWidget(widgets.MultiWidget):
-	def __init__(self, *args, **kwargs):
-		# attrs = {'style': 'width: 40px; text-align: center;'}
-		super().__init__([widgets.NumberInput, widgets.NumberInput], *args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        # attrs = {'style': 'width: 40px; text-align: center;'}
+        super().__init__([widgets.NumberInput,
+                          widgets.NumberInput], *args, **kwargs)
 
+    def format_output(self, rendered_widgets):
+        to = ' <span style="vertical-align: middle;"> -- </span> '
+        return rendered_widgets[0] + to + rendered_widgets[1]
 
-	def format_output(self, rendered_widgets):
-		to = ' <span style="vertical-align: middle;"> -- </span> '
-		return rendered_widgets[0] + to + rendered_widgets[1]
+    def decompress(self, value):
+        if value:
+            values = value.split('-')
 
-
-	def decompress(self, value):
-		if value:
-			values = value.split('-')
-
-			if len(values) > 1:
-				return values
-			if len(values) > 0:
-				return [values[0], values[0]]
-		return [None, None]
+            if len(values) > 1:
+                return values
+            if len(values) > 0:
+                return [values[0], values[0]]
+        return [None, None]
