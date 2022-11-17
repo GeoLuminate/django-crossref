2593ac327e8afcd5c104dff0600ae928cab6d91f
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-17 11:02:38 +0100

    refactoring

--- crossref/cms/forms.py
+++ crossref/cms/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from crossref.conf import settings
 from .models import Citation
 
+
 class CitationForm(forms.ModelForm):
-    # style = forms.ChoiceField(choices=settings.CROSSREF_CMS_STYLES)
+    # style = forms.ChoiceField(choices=
 
     class Meta:
         model = Citation
         fields = "__all__"
-        
