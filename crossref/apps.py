f382e7a69f4bc9f5d6d38fa29e3aa6e7cb1e0cd4
Sam Jennings <samuel.scott.jennings@gmail.com>
2022-11-17 11:05:23 +0100

    signals

--- crossref/apps.py
+++ crossref/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
+
 class CrossrefConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'crossref'
     verbose_name = 'Crossref'
+
+    def ready(self):
+        from . import signals
